8418d443a12158bb9a0a642e92ac70a3e90ff19b
deniseismo <denisseismo@gmail.com>
2021-02-14 18:22:52 +0700

    add a util function

---
+++ madnessbracket/utilities/spotify_utils.py
@@ -0,0 +1,18 @@
+import random
+
+
+def prepare_tracks(tracks: dict):
+    """
+    randomizes & caps (at 32) tracks/songs
+    :param tracks: a tracks dict with the list of 'track info' dicts
+    :return:
+    """
+    MAX_TRACKS = 32
+    # shuffles tracks
+    tracks['tracks'] = random.shuffle(tracks['tracks'])
+    # make sure it's divisible by 4 (madness bracket structure rule)
+    tracks_len = len(tracks['tracks']) // 4 * 4
+    # caps at 32 max or a lesser dividend
+    tracks_cap = min(32, tracks_len)
+    tracks['tracks'] = tracks['tracks'][:tracks_cap]
+    return tracks
