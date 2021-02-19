ff152bc1f552b100cf1fcab09e7aba19329e5b6b
deniseismo <denisseismo@gmail.com>
2021-02-19 11:40:12 +0700

    minor changes

--- madnessbracket/config.py
+++ madnessbracket/config.py
@@ -8,6 +8,9 @@ with open(os.path.join(file_path, 'config.json')) as config_file:
 
 class Config:
     SECRET_KEY = config.get('SECRET_KEY')
-    SERVER_NAME = '192.168.1.62:5000'
+    SERVER_NAME = config.get('SERVER_NAME')
     JSON_SORT_KEYS = False
     SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
+    SPOTIFY_CLIENT_SECRET = config.get("SPOTIFY_CLIENT_SECRET")
+    SPOTIFY_CLIENT_ID = config.get("SPOTIFY_CLIENT_ID")
+    SPOTIFY_REDIRECT_URI = config.get("SPOTIFY_REDIRECT_URI")
