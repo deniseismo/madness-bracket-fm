3584ab479e9f09f413533ccbfaf4088a7a599273
deniseismo <denisseismo@gmail.com>
2021-02-23 20:13:17 +0700

    add utils

---
+++ madnessbracket/static/js/utilities.js
@@ -0,0 +1,7 @@
+// Durstenfeld shuffle © https://www.wikiwand.com/en/Fisher%E2%80%93Yates_shuffle
+export function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
