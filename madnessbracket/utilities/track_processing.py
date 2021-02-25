72b5b5e45051b95fb3646ddd5cc68f8d4b6ee8d5
deniseismo <denisseismo@gmail.com>
2021-02-25 22:20:17 +0700

    change module name, add name filter function

---
+++ madnessbracket/utilities/track_processing.py
@@ -0,0 +1,48 @@
+import random
+import re
+
+
+def prepare_tracks(tracks: dict):
+    """
+    randomizes & caps (at 32) tracks/songs
+    :param tracks: a tracks dict with the list of 'track info' dicts
+    :return:
+    """
+    MAX_TRACKS = 32
+    # shuffles tracks
+    random.shuffle(tracks['tracks'])
+    # make sure it's divisible by 4 (madness bracket structure rule)
+    tracks_len = len(tracks['tracks']) // 4 * 4
+    # caps at 32 max or a lesser dividend
+    tracks_cap = min(32, tracks_len)
+    tracks['tracks'] = tracks['tracks'][:tracks_cap]
+    return tracks
+
+
+def get_filtered_name(name_to_fix: str):
+    """
+    :param name_to_fix: a song name to fix
+    :return: a fitlered name
+    """
+    # TODO: (mono/stereo) &/and
+    # replace some weird characters with normal ones
+    a_correct_title = name_to_fix.replace("’", "'")
+    patterns = [
+        # no special words in brackets
+        r"\(.*((\bremaster)|(\banniversary)|(\bEdition)|(\bmix)|(\bdeluxe)|(\bCD)|(\bsoundtrack)|(\bComplete)).*\)|\[.*\]",
+        # no super deluxe
+        r"((super)?\s?(deluxe)\s?).*",
+        r"(\-?\s?(\d+)?\s?(Remaster)\s?).*",
+        r"((\d+)?\s?(Bonus Tracks)\s?).*",
+        r"((\d+)?\s?(International Version)\s?).*",
+        r"\d+?(th)?\s?Anniversary\s?\w*",
+        r"(\s(\d+)?\s?(remix)\s?).*",
+        # no weird characters
+        r"[“”:\(\)\":…]"
+    ]
+    for pattern in patterns:
+        a_correct_title = re.sub(
+            pattern, '', a_correct_title, flags=re.IGNORECASE)
+    # finally remove some trailing hyphens and/or whitespaces
+    ultimate_filtered_name = a_correct_title.strip('-').strip()
+    return ultimate_filtered_name
