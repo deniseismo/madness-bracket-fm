8f1fd16c681103eaa43721077812f95a43e4c468
deniseismo <denisseismo@gmail.com>
2021-02-28 18:50:41 +0700

    rework routing: add bracket generator endpoint

--- madnessbracket/main/routes.py
+++ madnessbracket/main/routes.py
@@ -1,5 +1,7 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, jsonify, request, make_response
 from madnessbracket.spotify_api.spotify_user_oauth import check_spotify, get_spotify_user_info
+from madnessbracket.spotify_api.get_users_fav_tracks import get_users_favorite_tracks
+from madnessbracket.utilities.track_processing import cap_tracks
 
 main = Blueprint('main', __name__)
 
@@ -17,3 +19,43 @@ def home():
         logged_in = True
         user_info = get_spotify_user_info(token.access_token)
     return render_template("home.html", logged_in=logged_in, user_info=user_info)
+
+
+@main.route('/bracket', methods=['POST'])
+def generate_bracket():
+    """
+    handles the main routing/logic of the app
+    receieves user's input → returns prepared tracks accordingly
+    """
+    # input's values/options
+    content = request.get_json()
+    if not content:
+        print('no input')
+        return make_response(jsonify(
+            {'message': f"something's gone wrong"}
+        ),
+            404)
+    try:
+        tracks_type = content['type']
+        bracket_limit = content['limit']
+    except (KeyError, ValueError, TypeError):
+        print('bogus input')
+        return make_response(jsonify(
+            {'message': f"something's gone wrong"}
+        ),
+            404)
+    if tracks_type == "spotify":
+        tracks = get_users_favorite_tracks()
+    elif tracks_type == "best":
+        pass
+    elif tracks_type == "artist":
+        pass
+    else:
+        print('bogus tracks type')
+        return make_response(jsonify(
+            {'message': f"something's gone wrong"}
+        ),
+            404)
+
+    tracks = cap_tracks(tracks)
+    return jsonify(tracks)
