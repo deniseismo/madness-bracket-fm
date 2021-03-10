7b77d6cc4dec1933a4fdc953ba7cda6a2e9ae1fa
deniseismo <denisseismo@gmail.com>
2021-03-10 21:20:43 +0700

    fix artist spotify id getter: remove unnecessary "artist: " from the query; add 'spotipy' fallback function just in case

--- madnessbracket/dev/spotify/spotify_client_api.py
+++ madnessbracket/dev/spotify/spotify_client_api.py
@@ -1,9 +1,18 @@
 import random
 import tekore as tk
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 from flask import current_app
 
 from madnessbracket.utilities.track_processing import get_filtered_name
 
+
+def get_spotify_spotipy_client():
+    spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(
+        client_id=current_app.config['SPOTIFY_CLIENT_ID'],
+        client_secret=current_app.config['SPOTIFY_CLIENT_SECRET']
+    ))
+    return spotify
 
 
 def get_spotify_tekore_client():
@@ -52,16 +61,22 @@ def get_spotify_artist_id(artist_name: str, tekore_client=None):
     else:
         spotify_tekore_client = tekore_client
     spotify_tekore_client = get_spotify_tekore_client()
-    query = f"artist: {artist_name}"
-    artist_info, = spotify_tekore_client.search(query=query, types=('artist',))
+    query = f"{artist_name}"
+    artist_info, = spotify_tekore_client.search(
+        query=query, types=('artist',), limit=50, market="SE")
     # in case of not getting any response
     if not artist_info:
+        print("no info about the artist returned at all")
         return None
     # in case no items found
     if artist_info.total == 0:
+        print(artist_info)
+        print("no artists found")
         return None
     # iterate over artists found
-    for artist in artist_info.items:
+    print(artist_info.total)
+    for index, artist in enumerate(artist_info.items):
+        print(index, artist.name)
         # find the right one
         if artist.name.lower() == artist_name.lower():
             return artist.id
@@ -80,9 +95,39 @@ def get_spotify_artist_top_tracks(artist_name: str, tekore_client=None):
         spotify_tekore_client = get_spotify_tekore_client()
     else:
         spotify_tekore_client = tekore_client
-    artist_id = get_spotify_artist_id(artist_name, spotify_tekore_client)
-    top_tracks = spotify_tekore_client.artist_top_tracks(artist_id, 'RU')
+    # artist_id = get_spotify_artist_id(artist_name, spotify_tekore_client) # get ID via tekore
+    artist_id = spotipy_get_artist_id(artist_name)  # get ID via spotipy
+    print("artist spotify id", artist_id)
+    top_tracks = spotify_tekore_client.artist_top_tracks(artist_id, 'US')
     # in case of not getting any response
     if not top_tracks:
         return None
     return top_tracks
+
+
+def spotipy_get_artist_id(artist_name: str):
+    """
+    search for an artist's id
+    :param artist_name: artist's name
+    :return: album_image_url
+    """
+    spotify = get_spotify_spotipy_client()
+    try:
+        artist_info = spotify.search(
+            q=artist_name, type="artist", limit=5, market='SE')
+    except spotipy.exceptions.SpotifyException:
+        return None
+    if not artist_info:
+        return None
+    artist_id = None
+    print(artist_info['artists']['items'])
+    for item in artist_info['artists']['items']:
+        print(item['name'].lower(), artist_name.lower())
+        if item['name'].lower() == artist_name.lower():
+            try:
+                print('artists name are equal!')
+                artist_id = item['id']
+                break
+            except KeyError:
+                return None
+    return artist_id
