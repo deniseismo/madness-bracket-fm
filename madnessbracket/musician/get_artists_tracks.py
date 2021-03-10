6cf8eb72631cc28659e88de4510aa2139cfbdf0b
deniseismo <denisseismo@gmail.com>
2021-03-10 20:50:46 +0700

    make sure user's input is checked via last.fm: correct artist's name

--- madnessbracket/musician/get_artists_tracks.py
+++ madnessbracket/musician/get_artists_tracks.py
@@ -1,4 +1,6 @@
 from madnessbracket.models import Artist, Song
+from madnessbracket.dev.lastfm.lastfm_api import lastfm_get_artist_correct_name
+from sqlalchemy import func
 
 
 def get_artists_tracks(artist_name: str):
@@ -6,13 +8,27 @@ def get_artists_tracks(artist_name: str):
     :param artist_name: artist's name
     :return: a dict with a list of 'track info' dicts
     """
+    if not artist_name:
+        # no artist provided
+        return None
+    # correct user's input via lastfm's api
+    correct_name = lastfm_get_artist_correct_name(artist_name)
+    artist_name = correct_name if correct_name else artist_name
+    artist_name = artist_name.lower()
+
+    # set max song limit
     SONG_LIMIT = 50
-    artist = Artist.query.filter_by(name=artist_name).first()
+    # artist = Artist.query.filter_by(name=artist_name).first()
+    # find artist in the database
+    artist = Artist.query.filter(func.lower(
+        Artist.name) == artist_name).first()
     if not artist:
         # no such artist found
         return None
 
-    track_entries = Song.query.filter_by(artist=artist).order_by(Song.rating.desc()).limit(SONG_LIMIT).all()
+    # find top tracks in descending order (most listened first)
+    track_entries = Song.query.filter_by(artist=artist).order_by(
+        Song.rating.desc()).limit(SONG_LIMIT).all()
 
     if not track_entries:
         return None
