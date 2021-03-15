8642462bc7e2d60bee83868954df0a6d9b6a49d2
deniseismo <denisseismo@gmail.com>
2021-03-15 23:10:54 +0700

    add bracket description: artist's name or 'charts'

---
+++ madnessbracket/static/js/description.js
@@ -0,0 +1,11 @@
+import { createElement } from "./utilities.js";
+
+export function displayBracketDescription(description) {
+  const container = document.querySelector(".container");
+  const descriptionContainer = createElement("div", ["description-container"]);
+  const descriptionText = createElement("h1", ["description-text"]);
+  descriptionText.textContent = description.toUpperCase();
+  const hr = createElement("hr", ["description-line"]);
+  descriptionContainer.append(descriptionText, hr);
+  container.appendChild(descriptionContainer);
+}
