76e06ab1c68622072badeaaae1f60a9a945d1898
deniseismo <denisseismo@gmail.com>
2021-03-15 23:09:54 +0700

    add play/pause buttons, add text color (now dynamically contrast for better accessibility)

--- madnessbracket/static/js/bracketStructure.js
+++ madnessbracket/static/js/bracketStructure.js
@@ -2,12 +2,15 @@ import { Cell } from "./cell.js";
 import { bracket } from "./main.js";
 import { traverseAllCells } from "./bracketData.js";
 import { removeAllChildNodes, createElement } from "./utilities.js";
-import { getIcon } from "./icons.js";
-import { playMusic } from "./music.js";
+import { getIcon } from "./playButton.js";
+import { handlePlayButton } from "./music.js";
+import { displayBracketDescription } from "./description.js";
 
 export function createBracketStructure(tracksData) {
   const container = document.querySelector(".container");
+  const description = tracksData["description"];
   removeAllChildNodes(container);
+  displayBracketDescription(description);
   const tracksLength = tracksData["tracks"].length;
   bracket.setStructure(tracksLength);
   const tracks = {
@@ -41,7 +44,12 @@ export function createBracketStructure(tracksData) {
     // add a cell to the 'round' div 'tracksPerRound' times
     for (let j = 0; j < tracksPerRound; j++) {
       const cellClassName = `${side}-${roundIndex}-${j}`;
-      const cell = createElement("div", ["cell", cellClassName]);
+      const cellClassNameTwo = `cell-${side}`;
+      const cell = createElement("div", [
+        "cell",
+        cellClassName,
+        cellClassNameTwo,
+      ]);
       const songTitleElement = createElement("p", ["song-title"]);
       cell.appendChild(songTitleElement);
       const cellObject = new Cell(roundIndex, j, cell);
@@ -52,19 +60,23 @@ export function createBracketStructure(tracksData) {
         const previewURL = tracks[side][j]["spotify_preview_url"];
         if (previewURL) {
           const playIcon = getIcon("play");
-          const playButton = createElement("button", ["play-button"]);
+          const playButton = createElement("button", [
+            "play-button",
+            "play-icon_standby",
+          ]);
           playButton.appendChild(playIcon);
-          playButton.dataset.previewurl = previewURL;
-          playButton.addEventListener("click", (event) => {
+          playButton.dataset.status = "standby";
+          playButton.addEventListener("click", function (event) {
             event.preventDefault();
             event.stopPropagation();
-            playMusic(previewURL);
+            handlePlayButton.bind(playButton)(previewURL);
           });
           cell.appendChild(playButton);
         }
         const trackTitle = tracks[side][j]["track_title"];
         const artistName = tracks[side][j]["artist_name"];
         const albumColors = tracks[side][j]["album_colors"];
+        const textColor = tracks[side][j]["text_color"];
         console.log(trackTitle, artistName);
         cellObject.setCurrentSong(trackTitle);
         cellObject.setArtistName(artistName);
@@ -72,6 +84,7 @@ export function createBracketStructure(tracksData) {
         cellObject.makeAdvanceable();
         cellObject.activate();
 
+        cellObject.setTextColor(textColor);
         cellObject.setAlbumColors(albumColors);
         cellObject.applyColors();
       } else {
@@ -107,6 +120,7 @@ function createFinalRound() {
     "cell",
     "winner-cell",
     "cell_empty",
+    "final-cell",
   ]);
   const songWinnerTitleElement = createElement("p", ["song-title"]);
   winnerCell.appendChild(songWinnerTitleElement);
--- madnessbracket/static/js/music.js
+++ madnessbracket/static/js/music.js
@@ -1,7 +1,49 @@
+import { removeAllChildNodes } from "./utilities.js";
+import { getIcon } from "./playButton.js";
+
 export var music = new Audio();
-export function playMusic(url) {
+export function playMusic(url, button) {
   music.pause();
   music = new Audio(url);
   music.volume = 0.2;
   music.play();
+  music.onended = function () {
+    alert("audio playback has ended");
+  };
+}
+
+export function stopMusic() {
+  music.pause();
+}
+
+export function handlePlayButton(previewURL) {
+  console.log(this);
+  if (this.dataset.status === "standby") {
+    const allButtons = document.querySelectorAll(".play-button");
+    allButtons.forEach((button) => {
+      if (button.dataset.status !== "standby") {
+        removeAllChildNodes(button);
+        const playIcon = getIcon("play");
+        button.appendChild(playIcon);
+        button.dataset.status = "standby";
+        button.classList.remove("play-icon_playing");
+        button.classList.add("play-icon_standby");
+      }
+    });
+    removeAllChildNodes(this);
+    const pauseIcon = getIcon("pause");
+    this.appendChild(pauseIcon);
+    this.dataset.status = "playing";
+    this.classList.remove("play-icon_standby");
+    this.classList.add("play-icon_playing");
+    playMusic(previewURL, this);
+  } else {
+    removeAllChildNodes(this);
+    const playIcon = getIcon("play");
+    this.appendChild(playIcon);
+    this.classList.remove("play-icon_playing");
+    this.classList.add("play-icon_standby");
+    this.dataset.status = "standby";
+    stopMusic();
+  }
 }
