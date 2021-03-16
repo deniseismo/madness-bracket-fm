b8bde753936f49d653022142fae750e82fc6d563
deniseismo <denisseismo@gmail.com>
2021-03-16 23:49:44 +0700

    add figure support to svg generator

--- madnessbracket/static/js/svgGenerator.js
+++ madnessbracket/static/js/svgGenerator.js
@@ -2,11 +2,32 @@ export function getSVGIcon(svgData) {
   const svgIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
   svgIcon.classList.add("play-icon");
   svgIcon.setAttribute("viewBox", "0 0 512 512");
-  const aPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
-  const pathProperties = svgData.path;
-  for (const prop in pathProperties) {
-    aPath.setAttributeNS(null, prop, pathProperties[prop]);
+  const paths = svgData.path;
+  console.log("paths", paths);
+  for (const pathProperties of paths) {
+    console.log(pathProperties);
+    const aPath = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "path"
+    );
+    for (const prop in pathProperties) {
+      aPath.setAttributeNS(null, prop, pathProperties[prop]);
+    }
+    svgIcon.appendChild(aPath);
   }
-  svgIcon.appendChild(aPath);
+  if (svgData.hasOwnProperty("figures")) {
+    for (const figureProperties of svgData["figures"]) {
+      const aFigure = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        figureProperties["type"]
+      );
+      for (const prop in figureProperties) {
+        if (prop !== "type") {
+          aFigure.setAttributeNS(null, prop, figureProperties[prop]);
+        }
+      }
+      svgIcon.appendChild(aFigure);
+    }
+  }
   return svgIcon;
 }
