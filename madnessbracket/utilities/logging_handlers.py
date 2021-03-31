d9bee48ced2d1e9ef50972285c6271b51429e363
deniseismo <denisseismo@gmail.com>
2021-03-31 22:05:24 +0700

    fix logging

--- madnessbracket/utilities/logging_handlers.py
+++ madnessbracket/utilities/logging_handlers.py
@@ -1,10 +1,18 @@
+import os
 import csv
+from flask import current_app
 
 
 def log_missing_info(info):
     """
     logs missing info (missing songs/albums/releases, etc.) to a file
     """
-    with open('madnessbracket/dev/logs/missing_info.csv', 'a', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        writer.writerow([info])
+    filename = 'missing_info.csv'
+    filepath = os.path.join(current_app.root_path, 'dev/logs', filename)
+    try:
+        with open(filepath, 'a', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow([info])
+    except IOError as e:
+        print('file not found', e)
+        return None
