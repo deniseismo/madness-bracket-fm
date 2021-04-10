203bcd7061b98c1b8035f90f3d9d1885c0a02bef
deniseismo <denisseismo@gmail.com>
2021-04-10 16:53:30 +0700

    'cap' tracks differently: via weighted randomzation

--- madnessbracket/utilities/track_processing.py
+++ madnessbracket/utilities/track_processing.py
@@ -1,10 +1,12 @@
 import random
 import re
+from madnessbracket.utilities.randomize_track_selection import get_weighted_random_selection_of_tracks
 
 
 def cap_tracks(tracks: dict, limit: int = 16):
     """
     randomizes & caps (at a given limit, default=32) tracks/songs
+    :param limit: maximum number of tracks in a bracket
     :param tracks: a tracks dict with the list of 'track info' dicts
     :return: randomized & capped tracks
     """
@@ -12,21 +14,13 @@ def cap_tracks(tracks: dict, limit: int = 16):
     bracket_standards = [4, 8, 16, 32]
     # capping the standards with the limit given
     capped_standards = list(filter(lambda x: x <= limit, bracket_standards))
-    """
-    make a selection of tracks more relevant: that is if you limit tracks to 4 total, you expect them somewhat popular.
-    limit of 4 tracks will make us look only among top 8 (limit * 2) tracks, 8 → 16, 16 → 32, and so on.
-    ? This technique probably needs revision.
-    """
-    tracks['tracks'] = tracks['tracks'][:limit * 2]
-    # shuffles tracks
-    random.shuffle(tracks['tracks'])
     # get the total amount of tracks
-    tracks_len = len(tracks['tracks'])
-    print('total amount of tracks: ', tracks_len)
+    number_of_tracks = len(tracks['tracks'])
+    print('total amount of tracks: ', number_of_tracks)
     # figure out what the tracks cap should be: the closest maximum number out of the bracket standards
-    tracks_cap = max(list(filter(lambda x: x <= tracks_len, capped_standards)))
-    tracks['tracks'] = tracks['tracks'][:tracks_cap]
-    return tracks
+    tracks_cap = max(list(filter(lambda x: x <= number_of_tracks, capped_standards)))
+    randomized_tracks = get_weighted_random_selection_of_tracks(tracks['tracks'], tracks_cap)
+    return randomized_tracks
 
 
 def get_filtered_name(name_to_fix: str):
@@ -38,16 +32,18 @@ def get_filtered_name(name_to_fix: str):
     a_correct_title = fix_quot_marks(name_to_fix)
     patterns = [
         # no special words in brackets
-        r"\(.*((\bremaster)|(\banniversary)|(\bEdition)|(\bmix)|(\bdeluxe)|(\bCD)|(\bsoundtrack)|(\bComplete)|(\bRemix)).*\)|\[.*\]",
+        r"\(.*((\bremaster)|(\banniversary)|(\bEdition)|(\bmix)|(\bdeluxe)|(\bCD)|(\bsoundtrack)|(\bComplete)|(\bRemix)).*\)",
         r"(\-\s\w+\s(remix)\s?.*)",
         r"((super)?\s?(deluxe)\s?).*",
         r"(\-?\s?(\d+)?\s?(stereo|digital)?\s?(Remaster|acoustic)\s?).*",
+        r"((recorded)? live at .*)",
         r"((\d+)?\s?(Bonus Tracks)\s?).*",
         r"((\d+)?\s?(International Version)\s?).*",
         r"\d+?(th)?\s?Anniversary\s?\w*",
         r"(\s(\d+)?\s?(remix)\s?).*",
         r"((-\s.+)?\s(\d+)?\s?(original)?\s?(album|mono|stereo)?\s?(mix|version)).*",
         r"(- Live)",
+        r"(- .*edit)",
         r"(b-side)",
         r"((BBC)\s(.*)\s(session).*)",
         r"((MTV)\s.*)",
