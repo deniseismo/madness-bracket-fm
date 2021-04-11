8ac2784cf0f56ab75734f40e88d82934eac96aa1
deniseismo <denisseismo@gmail.com>
2021-04-11 21:20:36 +0700

    fix taking screenshots

--- src/js/share/takeScreenshot.js
+++ src/js/share/takeScreenshot.js
@@ -1,10 +1,19 @@
 import html2canvas from "html2canvas";
+import MicroModal from "micromodal";
 
 export function takeScreenshot() {
+  MicroModal.close("modal-1");
   html2canvas(document.querySelector(".wrapper")).then((canvas) => {
-    canvas.toBlob(function (blob) {
-      // Generate file download
-      window.saveAs(blob, "madnessbracket.png");
-    });
+    canvas.classList.add("screenshot");
+    document.body.addEventListener("click", closeScreenshot);
+    document.body.appendChild(canvas);
   });
+}
+
+function closeScreenshot() {
+  const screenshot = document.querySelector(".screenshot");
+  if (screenshot) {
+    screenshot.remove();
+  }
+  document.body.removeEventListener("click", closeScreenshot);
 }
