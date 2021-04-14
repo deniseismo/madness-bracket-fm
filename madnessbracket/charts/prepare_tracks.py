f3118bcf3b4548b70139be222bf1d2ea92e8a507
deniseismo <denisseismo@gmail.com>
2021-04-15 00:12:17 +0700

    prepare tracks for charts

---
+++ madnessbracket/charts/prepare_tracks.py
@@ -0,0 +1,17 @@
+import random
+from madnessbracket.utilities.track_processing import get_capped_bracket_size
+
+
+def prepare_tracks_for_charts(tracks: dict, limit=16):
+    """
+    randomizes & caps (at a given limit, default=32) tracks/songs
+    :param limit: maximum number of tracks in a bracket
+    :param tracks: a tracks dict with the list of 'track info' dicts
+    :return: randomized & capped tracks
+    """
+    number_of_tracks = len(tracks['tracks'])
+    print('total amount of tracks: ', number_of_tracks)
+    tracks_cap = get_capped_bracket_size(number_of_tracks, limit)
+    random.shuffle(tracks['tracks'])
+    tracks['tracks'] = tracks['tracks'][:tracks_cap]
+    return tracks
