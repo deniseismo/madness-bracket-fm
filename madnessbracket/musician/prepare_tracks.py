f2a65f30be9701c84a7adb86a14c88564e55d0ad
deniseismo <denisseismo@gmail.com>
2021-04-18 20:53:50 +0700

    make sure tracks from spotify are unique (no duplicate tracks)

--- madnessbracket/musician/prepare_tracks.py
+++ madnessbracket/musician/prepare_tracks.py
@@ -2,6 +2,7 @@ from madnessbracket.dev.db_mgmt.color_mgmt.dominant_colors import get_file_from_
 from madnessbracket.utilities.color_processing import get_contrast_color_for_two_color_gradient
 from madnessbracket.utilities.track_randomization import get_weighted_random_selection_of_tracks
 from madnessbracket.utilities.bracket_sizing import get_capped_bracket_size
+from madnessbracket.utilities.track_filtering import get_filtered_name
 
 
 def prepare_tracks_for_musician(tracks: dict, limit=16):
@@ -12,11 +13,15 @@ def prepare_tracks_for_musician(tracks: dict, limit=16):
     :param tracks: a tracks dict with the list of 'track info' dicts
     :return: randomized & capped tracks
     """
+    if not tracks:
+        return None
     number_of_tracks = len(tracks['tracks'])
     print('total amount of tracks: ', number_of_tracks)
     tracks_cap = get_capped_bracket_size(number_of_tracks, limit)
     randomized_tracks = get_weighted_random_selection_of_tracks(
         tracks['tracks'], tracks_cap)
+    if not randomized_tracks:
+        return None
     add_text_color_to_tracks(randomized_tracks)
     tracks['tracks'] = randomized_tracks
     return tracks
@@ -29,6 +34,8 @@ def add_text_color_to_tracks(tracks: list):
     :param tracks: a list of dicts with all the needed info about songs/tracks
     :return:
     """
+    if not tracks:
+        return None
     for track in tracks:
         if not track["album_colors"]:
             continue
@@ -36,7 +43,8 @@ def add_text_color_to_tracks(tracks: list):
         try:
             dominant_color = album_colors[0]
             secondary_color = album_colors[1]
-            text_color = get_contrast_color_for_two_color_gradient(dominant_color, secondary_color)
+            text_color = get_contrast_color_for_two_color_gradient(
+                dominant_color, secondary_color)
             track["text_color"] = text_color
         except (ValueError, IndexError) as e:
             print(e)
@@ -63,7 +71,12 @@ def process_tracks_from_db(tracks: list):
 
 def process_tracks_from_spotify(tracks: list):
     processed_tracks = []
+    track_titles = set()
     for track_entry in tracks:
+        track_title = get_filtered_name(track_entry.name)
+        if track_title in track_titles:
+            continue
+        track_titles.add(track_title)
         try:
             album_image_url = track_entry.album.images[-1].url
             image_file = get_file_from_url(album_image_url)
@@ -73,7 +86,7 @@ def process_tracks_from_spotify(tracks: list):
             print(e)
             album_colors = None
         track = {
-            "track_title": track_entry.name,
+            "track_title": get_filtered_name(track_entry.name),
             "artist_name": track_entry.artists[0].name,
             "spotify_preview_url": track_entry.preview_url if track_entry.preview_url else None,
             "album_colors": album_colors
