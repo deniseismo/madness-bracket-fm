da3be993ec2e96b00d1a95c32d095d4c6d30fdad
deniseismo <denisseismo@gmail.com>
2021-04-21 23:40:10 +0700

    change GET/POST for musician/artist: add URL search params

--- madnessbracket/musician/routes.py
+++ madnessbracket/musician/routes.py
@@ -1,41 +1,51 @@
-from flask import Blueprint, jsonify, request, make_response
+import json
+
+from flask import Blueprint, jsonify, request, make_response, render_template
 
 from madnessbracket.musician.musician_handlers import get_artists_tracks
-from madnessbracket.musician.prepare_tracks import prepare_tracks_for_musician
+from madnessbracket.utilities.user_input_validation import validate_artist_name, validate_bracket_upper_limit
+
 musician = Blueprint('musician', __name__)
 
 
-@musician.route('/artist', methods=['POST'])
+@musician.route('/artist', methods=['POST', "GET"])
 def generate_musician_bracket():
     """generates madness bracket for a particular artist/musician
     Returns:
         jsonified dict with all the tracks and tracks' info needed for the bracket
     """
-    # input's values/options
-    content = request.get_json()
-    if not content:
-        print('no input')
-        return make_response(jsonify(
-            {'message': f"something's gone wrong"}
-        ),
-            404)
-    try:
-        # get chosen bracket upper limit
-        bracket_limit = int(content['limit'])
-        # get chosen artist's name
-        artist_name = content['value']
-    except (KeyError, ValueError, TypeError):
-        print('bogus input')
-        return make_response(jsonify(
-            {'message': f"something's gone wrong"}
-        ),
-            404)
-    tracks = get_artists_tracks(artist_name, bracket_limit)
-    print(tracks)
-    if not tracks:
-        print('nothing found')
-        return make_response(jsonify(
-            {'message': f"no tracks found for {artist_name}"}
-        ),
-            404)
-    return jsonify(tracks)
+    if request.method == "GET":
+        artist_name = request.args.get("name")
+        upper_limit = request.args.get("limit")
+        valid_artist_name = validate_artist_name(artist_name)
+        valid_upper_limit = validate_bracket_upper_limit(upper_limit)
+        if not valid_artist_name or not valid_upper_limit:
+            return render_template('404.html', title='Incorrect Input'), 404
+
+        user_request = json.dumps({
+            "bracket_type": "artist",
+            "name": artist_name,
+            "limit": upper_limit
+        })
+        return render_template("bracket.html", user_request=user_request)
+    else:
+        artist_name = request.args.get("name")
+        upper_limit = request.args.get("limit")
+        valid_artist_name = validate_artist_name(artist_name)
+        valid_upper_limit = validate_bracket_upper_limit(upper_limit)
+        if not valid_artist_name or not valid_upper_limit:
+            print('no input')
+            return make_response(jsonify(
+                {'message': f"something's gone wrong"}
+            ),
+                404)
+        upper_limit = valid_upper_limit.upper_limit
+        tracks = get_artists_tracks(artist_name, upper_limit)
+        print(tracks)
+        if not tracks:
+            print('nothing found')
+            return make_response(jsonify(
+                {'message': f"no tracks found for {artist_name}"}
+            ),
+                404)
+        return jsonify(tracks)
