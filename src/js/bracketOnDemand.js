3f1f990110cc1cd7e889168912e3c8cc37a3785b
deniseismo <denisseismo@gmail.com>
2021-04-21 23:42:50 +0700

    construct bracket 'on demand': from url with correct search params

---
+++ src/js/bracketOnDemand.js
@@ -0,0 +1,67 @@
+import { BracketData } from "./bracket/bracketData.js";
+import { introAnimation } from "./animation/introAnimation.js";
+import { createBracketStructure } from "./bracket/bracketStructure.js";
+import { createIntroElements } from "./visuals/intro.js";
+import { OptionStorage } from "./options/optionStorage.js";
+import { addModal } from "./share/shareModal.js";
+import {
+  fetchTracks,
+  pushHistory,
+  constructQueryString,
+} from "./fetchTracks.js";
+import { showSpinner, hideSpinner } from "./visuals/spinner.js";
+export let bracket = new BracketData();
+
+export let options = new OptionStorage();
+
+// renders intro/header
+function getIntroHeaderReady() {
+  createIntroElements();
+  introAnimation();
+}
+
+getIntroHeaderReady();
+addModal();
+function getOptionsReady() {
+  options.setInputValue(userRequest["name"]);
+  options.setBracketMaxSize(userRequest["limit"]);
+  options.setCurrentBracketType(userRequest["bracket_type"]);
+}
+getOptionsReady();
+loadBracketOnDemand();
+
+function loadBracketOnDemand() {
+  showSpinner();
+  fetchTracks(options).then((data) => {
+    hideSpinner();
+    try {
+      if (data) {
+        const bracketType = options.getCurrentBracketType();
+        const name = options.getInputValue();
+        const limit = options.getBracketMaxSize();
+        const queryString = constructQueryString({
+          name: name,
+          limit: limit,
+        });
+        options.setCurrentTracks(data["tracks"]);
+        options.setDescription(data["description"]);
+        options.setSecret(data["secret"]);
+        createBracketStructure(bracket, options);
+        addModal();
+        pushHistory({
+          state: {
+            bracketType: bracketType,
+            name: name,
+            limit: limit,
+          },
+          title: null,
+          url: `http://192.168.1.62:5000/${bracketType}?` + queryString,
+        });
+      } else {
+        console.log(data);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  });
+}
