ded6ec5a985feab05c85ebdc866e36698fe5933c
deniseismo <denisseismo@gmail.com>
2021-06-12 15:13:07 +0700

    refactor autocomplete to be inline with the new version (10)

--- src/js/autocomplete/autocomplete.js
+++ src/js/autocomplete/autocomplete.js
@@ -3,13 +3,13 @@ import { checkScreenHeight } from "../misc/utilities.js";
 import { fetchArtists } from "./fetchArtists.js";
 
 // initialize autocomplete
-export function autocompleteInit() {
+export function setupAutocomplete(options) {
   // turn off autocomplete for smaller screens
   if (checkScreenHeight(700)) {
     return;
   }
   const autoCompleteJS = new autoComplete({
-    placeHolder: "Search for Food...",
+    placeHolder: "Search for Artist",
     data: {
       src: async () => {
         // Fetch Data from external Source
@@ -20,7 +20,7 @@ export function autocompleteInit() {
         return artistsFound;
       },
       cache: false,
-      results: (list) => {
+      filter: (list) => {
         const value = document.querySelector("#autoComplete").value.slice(0, 3);
         // Sort results by starting character
         const sortedList = list.sort((a, b) => {
@@ -38,45 +38,47 @@ export function autocompleteInit() {
       maxResults: checkScreenHeight(800) ? 1 : 3,
     },
     resultItem: {
-      highlight: {
-        render: true,
-      },
+      highlight: true,
     },
-    trigger: {
-      event: ["input", "focus"],
-    },
+    diacritics: "loose",
     threshold: 3,
-    onSelection: (feedback) => {
-      const inputField = document.querySelector("#autoComplete");
-      inputField.blur();
-      // Prepare User's Selected Value
-      const selection = feedback.selection.value;
-      // Render selected choice to selection div
-      // Replace Input value with the selected value
-      inputField.value = selection;
-      // Console log autoComplete data feedback
-      inputField.focus();
-      feedback.event.preventDefault();
-      document.querySelector(".form__group").requestSubmit();
-    },
   });
+  options.setAutocomplete(autoCompleteJS);
+}
+
+export function initAutocomplete(options) {
+  options.getAutocomplete().init();
+}
+
+export function unInitAutocomplete(options) {
+  options.getAutocomplete().unInit();
 }
 
 // additional autocomplete handlers
 export function handleAutocomplete() {
   // submit on enter
-  document
-    .querySelector("#autoComplete")
-    .addEventListener("keydown", (event) => {
-      if (event.key === "Enter") {
-        document.querySelector(".form__group").requestSubmit();
-      }
-    });
+  const autocompleteElement = document.querySelector("#autoComplete");
+  autocompleteElement.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      document.querySelector(".form__group").requestSubmit();
+    }
+  });
   // fill input's value with the selected value
-  document
-    .querySelector("#autoComplete")
-    .addEventListener("navigate", function (event) {
-      const selectedArtist = event.detail.selection.value;
-      document.querySelector("#autoComplete").value = selectedArtist;
-    });
+  autocompleteElement.addEventListener("navigate", function (event) {
+    const selectedArtist = event.detail.selection.value;
+    document.querySelector("#autoComplete").value = selectedArtist;
+  });
+  autocompleteElement.addEventListener("selection", function (event) {
+    const inputField = document.querySelector("#autoComplete");
+    inputField.blur();
+    // Prepare User's Selected Value
+    const selection = event.detail.selection.value;
+    // Render selected choice to selection div
+    // Replace Input value with the selected value
+    inputField.value = selection;
+    // Console log autoComplete data feedback
+    inputField.focus();
+    event.detail.event.preventDefault();
+    document.querySelector(".form__group").requestSubmit();
+  });
 }
