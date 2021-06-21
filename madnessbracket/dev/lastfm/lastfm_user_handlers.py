e35f2f7e008535c3b208518d4ff209b4a00eb744
deniseismo <denisseismo@gmail.com>
2021-06-21 22:09:03 +0700

    get correct username along with top tracks (lastfm)

--- madnessbracket/dev/lastfm/lastfm_user_handlers.py
+++ madnessbracket/dev/lastfm/lastfm_user_handlers.py
@@ -1,8 +1,11 @@
 import json
+import random
 
+from madnessbracket import cache
 from madnessbracket.dev.lastfm.lastfm_api import lastfm_get_response
 
 
+@cache.memoize(timeout=36000)
 def lastfm_get_user_top_tracks(username: str):
     """get user's top tracks on lastfm
 
@@ -24,15 +27,23 @@ def lastfm_get_user_top_tracks(username: str):
     response = lastfm_get_response({
         'method': 'user.getTopTracks',
         'user': username,
-        'period': "overall",
+        'period': random.choice(TIME_PERIODS),
         'limit': LIMIT
     })
     # in case of an error, return None
     if response.status_code != 200:
+        print('lastfm response NOT OK')
         return None
     try:
         top_tracks = response.json()["toptracks"]["track"]
-    except (KeyError, TypeError, json.decoder.JSONDecodeError):
+    except (KeyError, TypeError, json.decoder.JSONDecodeError) as e:
+        print(e)
+        return None
+    try:
+        correct_username = response.json()['toptracks']['@attr']['user']
+        username = correct_username
+    except (KeyError, ValueError) as e:
+        print(e)
         return None
     print(len(top_tracks))
-    return top_tracks
+    return username, top_tracks
