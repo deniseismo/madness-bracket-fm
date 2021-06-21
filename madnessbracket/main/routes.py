f61192ea80c428daa20720a6ab689603f17220c0
deniseismo <denisseismo@gmail.com>
2021-06-21 22:10:28 +0700

    check if user's logged in (spotify) in home route

--- madnessbracket/main/routes.py
+++ madnessbracket/main/routes.py
@@ -1,5 +1,7 @@
 from flask import render_template, Blueprint
 
+from madnessbracket.profile.spotify.spotify_profile_oauth import check_spotify_login, get_spotify_user_info
+
 main = Blueprint('main', __name__)
 
 
@@ -11,7 +13,12 @@ def home():
     Returns:
         home page template (home.html)
     """
-    return render_template("home.html")
+    user, token = check_spotify_login()
+    spotify_info = {
+        "logged_in": user and token,
+        "user_info": get_spotify_user_info(token.access_token) if (user and token) else None
+    }
+    return render_template("home.html", spotify_info=spotify_info)
 
 
 @main.route("/about", methods=["GET"])
