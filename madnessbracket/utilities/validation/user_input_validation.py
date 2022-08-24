d7011a9290fe5ac989a86d0a66c634d358f9ee14
deniseismo <denisseismo@gmail.com>
2022-08-24 22:25:27 +0700

    add validating functions for user input

---
+++ madnessbracket/utilities/validation/user_input_validation.py
@@ -0,0 +1,106 @@
+from typing import Optional
+
+from pydantic import ValidationError
+from werkzeug.datastructures import ImmutableMultiDict
+
+from madnessbracket.schemas.input import LastFMUserInput, ArtistUserInput, ArtistsBattleUserInput
+from madnessbracket.utilities.validation.exceptions.validation_exceptions import LastFMUserInputError, \
+    ArtistUserInputError, ArtistsBattleUserInputError, BracketUpperLimitError
+from madnessbracket.utilities.validation.validation_models import LastFMUsername, BracketUpperLimit, ArtistName
+
+
+def validate_lastfm_user_input(user_input: ImmutableMultiDict) -> Optional[LastFMUserInput]:
+    """
+    validate user input for lastfm user handlers (username, bracket upper limit)
+    :param user_input: (ImmutableMultiDict) user's input with username and bracket limit
+    :return: (LastFMUserInput) validated user input
+    """
+    if not isinstance(user_input, ImmutableMultiDict):
+        raise LastFMUserInputError("incorrect input type")
+
+    lastfm_username = user_input.get("name")
+    bracket_upper_limit = user_input.get("limit")
+    if not lastfm_username:
+        raise LastFMUserInputError("no username provided")
+    if not bracket_upper_limit:
+        raise LastFMUserInputError("no bracket limit provided")
+    try:
+        valid_lastfm_username = LastFMUsername(name=lastfm_username).name
+        valid_bracket_upper_limit = BracketUpperLimit(limit=bracket_upper_limit).limit
+        return LastFMUserInput(name=valid_lastfm_username, limit=valid_bracket_upper_limit)
+    except ValidationError as e:
+        print(e.json())
+        raise LastFMUserInputError("incorrect username")
+
+
+def validate_artist_user_input(user_input: ImmutableMultiDict) -> Optional[ArtistUserInput]:
+    """
+    validate user input for artist/musician handlers (artist's name, bracket upper limit)
+    :param user_input: (ImmutableMultiDict) user's input with artist's name and bracket limit
+    :return: (ArtistUserInput) validated user input
+    """
+    if not isinstance(user_input, ImmutableMultiDict):
+        raise ArtistUserInputError("incorrect input type")
+    artist_name = user_input.get("name")
+    bracket_upper_limit = user_input.get("limit")
+    if not artist_name:
+        raise ArtistUserInputError("no artist provided")
+    if not bracket_upper_limit:
+        raise ArtistUserInputError("no bracket limit provided")
+
+    try:
+        valid_artist_name = ArtistName(name=artist_name).name
+        valid_bracket_upper_limit = BracketUpperLimit(limit=bracket_upper_limit).limit
+        return ArtistUserInput(name=valid_artist_name, limit=valid_bracket_upper_limit)
+    except ValidationError as e:
+        print(e.json())
+        raise ArtistUserInputError("incorrect artist name")
+
+
+def validate_artists_battle_user_input(user_input: ImmutableMultiDict) -> Optional[ArtistsBattleUserInput]:
+    """
+    validate user input for artists battle (artists names, bracket limit)
+    :param user_input: (ImmutableMultiDict) user's input with artists names and bracket limit
+    :return: (ArtistsBattleUserInput) validated user input
+    """
+    if not isinstance(user_input, ImmutableMultiDict):
+        raise ArtistUserInputError("incorrect input type")
+    artist_name = user_input.get("name")
+    artist_2_name = user_input.get("name2")
+    bracket_upper_limit = user_input.get("limit")
+    if not artist_name or not artist_2_name:
+        raise ArtistsBattleUserInputError("no artist provided")
+    if not bracket_upper_limit:
+        raise ArtistsBattleUserInputError("no bracket limit provided")
+
+    try:
+        valid_artist_name = ArtistName(name=artist_name).name
+        valid_artist_2_name = ArtistName(name=artist_2_name).name
+        valid_bracket_upper_limit = BracketUpperLimit(limit=bracket_upper_limit).limit
+        if valid_artist_name.lower() == valid_artist_2_name.lower():
+            raise ArtistsBattleUserInputError("IT TAKES TWO TO BATTLE")
+        return ArtistsBattleUserInput(name=valid_artist_name, name2=valid_artist_2_name,
+                                      limit=valid_bracket_upper_limit)
+    except ValidationError as e:
+        print(e.json())
+        raise ArtistsBattleUserInputError("incorrect artist name")
+
+
+def validate_bracket_upper_limit(user_input: ImmutableMultiDict) -> Optional[int]:
+    """
+    validate bracket upper limit
+    :param user_input: (ImmutableMultiDict) user's input with bracket upper limit
+    :return: (int) validated bracket upper limit
+    """
+    if not isinstance(user_input, ImmutableMultiDict):
+        raise ArtistUserInputError("incorrect input type")
+    bracket_upper_limit = user_input.get("limit")
+    if not bracket_upper_limit:
+        raise BracketUpperLimitError("no bracket limit provided")
+
+    try:
+        valid_bracket_upper_limit = BracketUpperLimit(limit=bracket_upper_limit).limit
+        return valid_bracket_upper_limit
+    except ValidationError as e:
+        print(e.json())
+        raise BracketUpperLimitError("incorrect bracket upper limit")
