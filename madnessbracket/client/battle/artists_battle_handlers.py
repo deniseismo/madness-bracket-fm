91304d52a132f6db1803ed49b92ecb5420d2dd60
deniseismo <denisseismo@gmail.com>
2022-08-25 21:38:06 +0700

    refactor artists battle

--- madnessbracket/client/battle/artists_battle_handlers.py
+++ madnessbracket/client/battle/artists_battle_handlers.py
@@ -1,39 +1,31 @@
 from typing import Optional
 
-from madnessbracket.battle.prepare_tracks import prepare_tracks_for_artist_battle
-from madnessbracket.musician.musician_handlers import get_artists_tracks
+from madnessbracket.track_processing.track_preparation import prepare_tracks_for_artist_battle
+from madnessbracket.client.musician.musician_handlers import get_artists_tracks
+from madnessbracket.schemas.track_schema import TrackInfo
 
 
-def get_artists_battle(artist_name: str, artist_name_2: str, bracket_limit: int) -> Optional[dict]:
+def get_tracks_for_artists_battle(
+        artist_1_name: str,
+        artist_2_name: str,
+        bracket_limit: int
+) -> Optional[list[TrackInfo]]:
     """
     get tracks for ARTIST BATTLE: ARTIST 1 vs ARTIST 2 (e.g. Radiohead vs Muse)
-    :param artist_name: first artist's name
-    :param artist_name_2: second artist's name
-    :param bracket_limit: chosen bracket upper limit
-    :return: a dict with all the information needed for artists battle
+    :param artist_1_name: (str) first artist's name
+    :param artist_2_name: (str) second artist's name
+    :param bracket_limit: (int) chosen bracket upper limit
+    :return: (list[TrackInfo]) a list of TrackInfo with all the information needed for artists battle
     """
-    if not artist_name or not artist_name_2 or not bracket_limit:
+    if not artist_1_name or not artist_2_name or not bracket_limit:
         return None
-    artist_1_tracks = get_artists_tracks(artist_name, bracket_limit)
+    artist_1_tracks = get_artists_tracks(artist_1_name, bracket_limit)
     if not artist_1_tracks:
-        print(f"could not find tracks for {artist_name}")
+        print(f"could not find tracks for {artist_1_name}")
         return None
-    artist_2_tracks = get_artists_tracks(artist_name_2, bracket_limit)
+    artist_2_tracks = get_artists_tracks(artist_2_name, bracket_limit)
     if not artist_2_tracks:
-        print(f"could not find tracks for {artist_name_2}")
+        print(f"could not find tracks for {artist_2_name}")
         return None
-    battle_tracks = prepare_tracks_for_artist_battle(
-        artist_1_tracks, artist_2_tracks, bracket_limit)
-    artist_1_corrected_name = artist_1_tracks[0]["artist_name"]
-    artist_2_corrected_name = artist_2_tracks[0]["artist_name"]
-    description = f"{artist_1_corrected_name.upper()} vs {artist_2_corrected_name.upper()}"
-    if artist_1_corrected_name == artist_2_corrected_name:
-        description += ": An Internal Conflict"
-    tracks = {
-        "tracks": battle_tracks,
-        "description": description,
-        "value1": artist_1_corrected_name,
-        "value2": artist_2_corrected_name,
-        "extra": "artists_battle"
-    }
-    return tracks
+    battle_tracks = prepare_tracks_for_artist_battle(artist_1_tracks, artist_2_tracks, bracket_limit)
+    return battle_tracks
