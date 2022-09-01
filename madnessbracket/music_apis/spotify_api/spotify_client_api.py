c2302f8036293a608ff282e40e75b0ae9760ad63
deniseismo <denisseismo@gmail.com>
2022-09-01 21:46:46 +0700

    refactor spotify user handlers

--- madnessbracket/music_apis/spotify_api/spotify_client_api.py
+++ madnessbracket/music_apis/spotify_api/spotify_client_api.py
@@ -1,7 +1,10 @@
+from typing import Optional
+
 import spotipy
 import tekore as tk
 from flask import current_app
 from spotipy.oauth2 import SpotifyClientCredentials
+from tekore import ServiceUnavailable
 
 from madnessbracket.utilities.helpers import timeit
 
@@ -20,16 +23,20 @@ def get_spotify_spotipy_client():
 
 
 @timeit
-def get_spotify_tekore_client(asynchronous=False):
-    """get a spotify client via tekore library
-
-    Returns:
-        an instance of a Spotify client
+def get_spotify_tekore_client(asynchronous: bool = False) -> Optional[tk.Spotify]:
+    """
+    get spotify tekore library client
+    :param asynchronous: async type of client
+    :return: (tk.Spotify) Spotify tekore client
     """
     client_id = current_app.config['SPOTIFY_CLIENT_ID']
     client_secret = current_app.config['SPOTIFY_CLIENT_SECRET']
-
-    app_token = tk.request_client_token(
-        client_id=client_id, client_secret=client_secret)
-    spotify_tekore_client = tk.Spotify(app_token, asynchronous=asynchronous)
+    try:
+        app_token = tk.request_client_token(
+            client_id=client_id, client_secret=client_secret)
+        spotify_tekore_client = tk.Spotify(app_token, asynchronous=asynchronous)
+    except ServiceUnavailable as e:
+        print("can't get spotify tekore client")
+        print(e)
+        return None
     return spotify_tekore_client
---
+++ madnessbracket/music_apis/spotify_api/spotify_user_handlers.py
@@ -0,0 +1,147 @@
+import pickle
+import random
+from typing import Optional
+
+import tekore as tk
+from flask import current_app, session
+
+from madnessbracket.models import User
+from madnessbracket.music_apis.spotify_api.spotify_client_api import get_spotify_tekore_client
+from madnessbracket.schemas.spotify_user_info import SpotifyUserAuth, SpotifyUserProfile, SpotifyTopTracksInfo, \
+    SpotifyTopTracksProcessedInfo
+from madnessbracket.track_processing.process_tracks_from_spotify import process_tracks_from_spotify
+
+
+def get_spotify_auth() -> tk.UserAuth:
+    """
+    get a tekore spotify User auth object
+    :return: (tk.UserAuth)
+    """
+    conf = _get_app_spotify_credentials()
+    cred = tk.Credentials(*conf)
+    # scopes allow client to read user's name, id, avatar & user's top artists/tracks
+    scope = tk.Scope(tk.scope.user_top_read, tk.scope.user_read_private)
+    auth = tk.UserAuth(cred, scope)
+    return auth
+
+
+def _get_app_spotify_credentials() -> tuple[str, str, str]:
+    """
+    get spotify client credentials [client id, client secret, redirect uri]
+    :return: (tuple[str, str, str]) spotify credentials [client id, client secret, redirect uri]
+    """
+    return (
+        current_app.config['SPOTIFY_CLIENT_ID'],
+        current_app.config['SPOTIFY_CLIENT_SECRET'],
+        current_app.config['SPOTIFY_REDIRECT_URI']
+    )
+
+
+def authenticate_spotify_user() -> Optional[SpotifyUserAuth]:
+    """
+    checks/authenticates spotify user; refreshes token if present
+    :return: (SpotifyUserAuth) with spotify user id and access token
+    """
+    user = session.get('user', None)
+    token = session.get('token', None)
+    if token:
+        token = pickle.loads(session.get('token', None))
+
+    if not (user and token):
+        session.pop('user', None)
+        session.pop('token', None)
+        return SpotifyUserAuth(None, None)
+
+    if token.is_expiring:
+        # get new access token
+        conf = _get_app_spotify_credentials()
+        cred = tk.Credentials(*conf)
+        user_entry = User.query.filter_by(spotify_id=user).first()
+        if user_entry:
+            # get user's refresh token from db
+            refresh_token = user_entry.spotify_token
+            if refresh_token:
+                # get new token via refresh token
+                token = cred.refresh_user_token(refresh_token)
+                session['token'] = pickle.dumps(token)
+
+    return SpotifyUserAuth(user, token)
+
+
+def get_spotify_user_info(token) -> Optional[SpotifyUserProfile]:
+    """
+    get information about the current spotify user
+    :param token: a Spotify access token
+    :return: (SpotifyUserProfile) with spotify user info (username, avatar, country)
+    """
+    spotify_tekore_client = get_spotify_tekore_client()
+    if not spotify_tekore_client:
+        return None
+    try:
+        with spotify_tekore_client.token_as(token):
+            current_user = spotify_tekore_client.current_user()
+
+            username = current_user.display_name
+            country = current_user.country
+            user_image = None
+            current_user_image_list = current_user.images
+            if current_user_image_list:
+                try:
+                    user_image = current_user.images[0].url
+                except (IndexError, TypeError) as e:
+                    print(e)
+    except tk.HTTPError:
+        return None
+    return SpotifyUserProfile(username=username, user_image=user_image, country=country)
+
+
+def spotify_get_users_top_tracks(token) -> Optional[SpotifyTopTracksProcessedInfo]:
+    """
+    get all the best spotify user's tracks for a randomly chosen time period
+    :param token: spotify user access token
+    :return: (SpotifyTopTracksProcessedInfo) with username & all the tracks processed
+    """
+    if not token:
+        return None
+    # spotify's internal 'time spans': gives different selections of your fav. tracks based on time period
+    time_ranges = ['short_term', 'medium_term', 'long_term']
+    time_range = random.choice(time_ranges)
+    spotify_top_tracks_info = _spotify_get_top_tracks_for_specific_time_range(token, time_range)
+    if not spotify_top_tracks_info:
+        return None
+    username, spotify_user_top_tracks = spotify_top_tracks_info
+    processed_tracks = process_tracks_from_spotify(spotify_user_top_tracks)
+    return SpotifyTopTracksProcessedInfo(username=username, tracks=processed_tracks)
+
+
+def _spotify_get_top_tracks_for_specific_time_range(token, time_range: str) -> Optional[SpotifyTopTracksInfo]:
+    """
+    get spotify user's best tracks for a specific time period (short term, medium term, long term)
+    :param token: spotify user access token
+    :param time_range: (str) one of the spotify's internal time spans: (['short_term', 'medium_term', 'long_term'])
+    :return: (SpotifyTopTracksInfo) with username & FullTrack tracks (unprocessed spotify tekore track objects)
+    """
+    spotify_tekore_client = get_spotify_tekore_client()
+    if not spotify_tekore_client:
+        return None
+    try:
+        with spotify_tekore_client.token_as(token):
+            current_user = spotify_tekore_client.current_user()
+            username = current_user.display_name
+            # get user's top 50 tracks (pick time span at random)
+            top_tracks = spotify_tekore_client.current_user_top_tracks(
+                limit=50,
+                time_range=time_range
+            )
+    except tk.HTTPError:
+        return None
+
+    if not top_tracks:
+        return None
+
+    if not top_tracks.items:
+        return None
+    if len(top_tracks.items) < 4:
+        print('not enough tracks')
+        return None
+    return SpotifyTopTracksInfo(username=username, tracks=top_tracks.items)
