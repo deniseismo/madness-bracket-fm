9193c0adcc1edb4f826ad154fa966a9c1ebefe28
deniseismo <denisseismo@gmail.com>
2022-10-30 03:09:23 +0600

    add 'delete user from db' function

--- madnessbracket/client/database_manipulation/db_user_handlers.py
+++ madnessbracket/client/database_manipulation/db_user_handlers.py
@@ -12,3 +12,16 @@ def add_new_spotify_user_to_database(spotify_user_id: str, refresh_token: str) -
     db.session.add(user_entry)
     db.session.commit()
     return True
+
+
+def delete_spotify_user_from_database(spotify_user_id: str) -> bool:
+    """
+    delete spotify user from database (by spotify user id)
+    :param spotify_user_id: user's id on Spotify
+    """
+    user_entry = User.query.filter_by(spotify_id=spotify_user_id).first()
+    if not user_entry:
+        return False
+    db.session.delete(user_entry)
+    db.session.commit()
+    return True
