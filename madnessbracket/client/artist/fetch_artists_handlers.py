d497210437c6eb2054e37feb707b8b8b4a7f3865
deniseismo <denisseismo@gmail.com>
2022-11-15 22:25:07 +0600

    separate loading all artists

--- madnessbracket/client/artist/fetch_artists_handlers.py
+++ madnessbracket/client/artist/fetch_artists_handlers.py
@@ -1,26 +1,37 @@
 import json
 import os
+from typing import Optional
 
 from flask import current_app
 
 from madnessbracket import cache
 
 
+@cache.memoize(timeout=60000)
+def _load_all_artists():
+    """
+    load a list of artists from disk
+    :return:
+    """
+    ARTISTS_LIST_FILENAME = "all_artists.json"
+    ARTISTS_LIST_PATH = os.path.join(current_app.root_path, "client", "artist", ARTISTS_LIST_FILENAME)
+    try:
+        with open(ARTISTS_LIST_PATH) as jsonfile:
+            music_genres_list = json.load(jsonfile)
+    except (IOError, OSError) as e:
+        print(e)
+        return []
+    return music_genres_list
+
+
 @cache.memoize(timeout=36000)
-def get_filtered_artists_suggestions(query) -> list:
+def get_filtered_artists_suggestions(query: Optional[str]) -> list[str]:
     """get appropriate artists suggestions according to the given search input (query)
 
     Args:
         query (str): user's input
     """
-    try:
-        filename = "all_artists.json"
-        filepath = os.path.join(current_app.root_path, "client", "artist", filename)
-        with open(filepath, encoding="UTF-8") as file:
-            artists_list = json.load(file)
-    except (IOError, OSError) as e:
-        print(e)
-        return []
+    artists_list = _load_all_artists()
     print(query)
     filtered_artists_list = []
     # check if the input's not empty
